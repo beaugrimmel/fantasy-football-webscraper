53f2701fac1be419c2bd08af04e110b151ee0c9e
Beau Grimmel <beaugrimmel@gmail.com>
2020-12-04 16:38:18 -0500

    Finish fixing methods

--- free_agent_gods.py
+++ free_agent_gods.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
+from tabulate import tabulate
+from secrets import username, password
 
 class Player():
     def __init__(self):
@@ -12,6 +14,12 @@ class Player():
         self.points = ''
         self.projection = ''
         self.position = ''
+    
+    def __eq__(self, other):
+        return self.name==other.name
+    
+    def __hash__(self):
+        return hash(self.name)
 
 class Webscraper():
     def __init__(self, chromedriver_path):
@@ -25,65 +33,41 @@ class Webscraper():
         self.driver.get(website_url)
 
     def login(self, username, password):
-        try:
-            user_box = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="landing"]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/input')))
-            user_box.send_keys(username)
-            user_box.send_keys(Keys.RETURN)
-        except TimeoutException:
-            print ("Loading took too much time for user box!")
-        try:
-            password_box = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="landing"]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/input')))
-            password_box.send_keys(password)
-            password_box.send_keys(Keys.RETURN)
-        except TimeoutException:
-            print ("Loading took too much time for password box!")
+        user_box = self.driver.find_element_by_xpath('//*[@id="landing"]/div/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/input')
+        user_box.send_keys(username)
+        user_box.send_keys(Keys.RETURN)
 
-    def select_leaders_tab(self):
-        try:
-            leaders_box = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[1]/a[3]')))
-            leaders_box.click()
-        except:
-            print ("Loading took too much time for leaders tab!")
+        password_box = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="landing"]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/input')))
+        password_box.send_keys(password)
+        password_box.send_keys(Keys.RETURN)
 
     def select_league(self):
-        pass # TODO Implement function to select leagueDO 
+        pass # TODO Implement function to select league
+
+    def select_leaders_tab(self):
+        leaders_box = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[1]/a[3]')))
+        leaders_box.click()
 
     def select_week(self, week):
-        # TODO This just doesnt work well
-        try:
-            week_dropdown = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div[2]/div[2]')))
-        except TimeoutException:
-            print ("Loading took too much time for week dropdown!")
-            return
+        week_dropdown = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div[2]/div[2]')))
         week_dropdown.click()
-        try:
-            week_number = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div[{}]'.format(week + 1))))
-        except TimeoutException:
-            print ("Loading took too much time for week number!")
-            return
+
+        week_number = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div[{}]'.format(week + 1))))
         week_number.click()
 
     def select_stat_type(self, type):
-        # TODO Not working for Projection
+        # TODO Look into why theres sometimes timeout issues
         if type == 'Projection':
             stat_num = 1
         elif type == 'Stats':
             stat_num = 2
-            return # Set to stats by default
         else:
             raise ValueError("Must enter Projection or Stats")
-        try:
-            stat_type_button = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[{}]'.format(stat_num))))
-        except:
-            raise TimeoutException("Loading took too much time for stat type!")
+        stat_type_button = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[{}]'.format(stat_num))))
         stat_type_button.click()
 
     def hide_owned_players(self):
-        time.sleep(1)
-        try:
-            hide_owned = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div[3]/div[2]')))
-        except TimeoutException:
-            print ("Loading took too much time for hide owned!")
+        hide_owned = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[2]/div[3]/div[2]')))
         hide_owned.click()
 
     def select_player_type(self, player_type):
@@ -99,51 +83,61 @@ class Webscraper():
                 'ROOKIE':(8,2)
             }
         type_num = switcher.get(player_type, 'Invalid player type')
-        try:
-            player_type = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div[{}]'.format(type_num[0]))))
-            player_type.click()
-        except:
-            print ("Loading took too much time for player type!")
+
+        player_type = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div[{}]'.format(type_num[0]))))
+        player_type.click()
+
         if type_num[0] == 8:
-            try:
-                player_type_2 = WebDriverWait(self.driver, 25).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div[8]/div/div/div[{}]'.format(1))))
-                player_type_2.click()
-            except:
-                print ("Loading took too much time for player type part 2!")
+            player_type_2 = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[3]/div[1]/div[2]/div[2]/div/div[8]/div/div/div[{}]'.format(type_num[1]))))
+            player_type_2.click()
 
     def get_top_x_player(self, rank):
         player = Player()
-        try:
-            player_name = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[2]/div[2]/div[1]'.format(rank))))
-            player_points = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[6]'.format(rank))))
-            player_projection = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[5]'.format(rank))))
-            player_position = WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[2]/div[3]/div[1]'.format(rank))))
-        except TimeoutException:
-            print ("Loading took too much time for select player!")
+        player_name = WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[2]/div[2]/div[1]'.format(rank))))
+        player_points = WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[6]'.format(rank))))
+        player_projection = WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[5]'.format(rank))))
+        player_position = WebDriverWait(self.driver, 3).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[3]/div/div[2]/div/div[5]/div/div[1]/div/div[1]/div/div/div[{}]/div/div[2]/div[3]/div[1]'.format(rank))))
         player.name = player_name.text
         player.points = player_points.text
         player.projection = player_projection.text
         player.position = player_position.text
         return player
 
-def make_list_top_players(scraper, playertype, top_x):
-    scraper.select_player_type(playertype)
-    players = []
-    for count in range(top_x):
-        players.append(scraper.get_top_x_player(rank=(count+1)))
-    return players
+    def make_list_top_players(self, playertype, top_x):
+        scraper.select_player_type(playertype)
+        players = []
+        for count in range(top_x):
+            players.append(scraper.get_top_x_player(rank=(count+1)))
+        return players
 
 if __name__ == "__main__":
     scraper = Webscraper('/Users/beaugrimmel/Documents/GitHub/fantasy-football-webscraper/chromedriver')
     scraper.goto_website('https://sleeper.app/login')
-    scraper.login('', '')
+    scraper.login(username, password)
     scraper.select_leaders_tab()
-    # scraper.select_stat_type(type='Stats')
-    # scraper.select_week(week=9)
+    scraper.select_week(week=12)
     scraper.hide_owned_players()
+    scraper.select_stat_type(type='Stats')
 
-    rbs = make_list_top_players(scraper, 'RB', 2)
-    print(rbs[1].name)
-    print(rbs[1].points)
+    qb = scraper.make_list_top_players('QB', 1)
+    rb = scraper.make_list_top_players('RB', 2)
+    wr = scraper.make_list_top_players('WR', 2)
+    te = scraper.make_list_top_players('TE', 1)
+    flex = scraper.make_list_top_players('FLEX', 7)
+    k = scraper.make_list_top_players('K', 1)
+    d = scraper.make_list_top_players('DEF', 1)
+    starters = qb + rb + wr + te + flex + k + d
 
+    starters = list(dict.fromkeys(starters))
+
+    # Print results nicely formatted
+    starters_print = []
+    total_points = 0
+    for starter in starters:
+        starters_print.append([starter.name, starter.position, starter.points])
+        total_points += round(float(starter.points),2)
+    print(tabulate(starters_print, headers=['Name', 'Pos/Team', 'Points']))
+    print('Total points: {}'.format(total_points))
+
+    time.sleep(2.5)
     scraper.end_scraper()
